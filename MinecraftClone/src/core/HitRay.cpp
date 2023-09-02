23557b09d58018688ba2ce054a946eff16a90ab8
Jukitsu <ninjax2312@gmail.com>
2023-09-02 21:27:12 +0200

    Actually working hitray I guess

--- MinecraftClone/src/core/HitRay.cpp
+++ MinecraftClone/src/core/HitRay.cpp
@@ -2,7 +2,7 @@
 #include "core/HitRay.h"
 
 HitRay::HitRay(World& world, Camera& camera) 
-	:world(world), position(camera.getPos()), block((glm::ivec3)glm::floor(camera.getPos())), distance(0.0f) {
+	:world(world), position(camera.getPos() - glm::vec3(0.5f)), block((glm::ivec3)glm::round(camera.getPos() - glm::vec3(0.5f))), distance(0.0f) {
 	vector = glm::vec3(
 		glm::cos(camera.getYaw()) * glm::cos(camera.getPitch()),
 		glm::sin(camera.getPitch()),
@@ -26,7 +26,7 @@ bool HitRay::check(int button, HitCallback callback,
 	}
 }
 bool HitRay::step(int button, HitCallback callback) {
-	glm::ivec3 sign = { 1, 1, 1 };
+	glm::ivec3 sign = glm::ivec3(1, 1, 1);
 	glm::vec3 localPos = position - (glm::vec3)block;
 
 	glm::vec3 absoluteVector = vector;
@@ -45,33 +45,33 @@ bool HitRay::step(int button, HitCallback callback) {
 				
 
 	if (vx) {
-		float x = 1.0f;
-		float y = (1.0f - lx) / vx * vy + ly;
-		float z = (1.0f - lx) / vx * vz + lz;
+		float x = 0.5f;
+		float y = (0.5f - lx) / vx * vy + ly;
+		float z = (0.5f - lx) / vx * vz + lz;
 
-		if (y >= 0.0f && y <= 1.0f && z >= 0.0f && z <= 1.0f) {
+		if (y >= -0.5f && y <= 0.5f && z >= -0.5f && z <= 0.5f) {
 			float distance = glm::distance(glm::vec3(x, y, z), localPos);
 			return check(button, callback, distance, block, glm::vec3(block.x + sign.x, block.y, block.z));
 		}
 	}
 		
 	if (vy) {
-		float x = (1.0f - ly) / vy * vx + lx;
-		float y = 1.0f;
-		float z = (1.0f - ly) / vy * vz + lz;
+		float x = (0.5f - ly) / vy * vx + lx;
+		float y = 0.5f;
+		float z = (0.5f - ly) / vy * vz + lz;
 
-		if (x >= 0.0f && x <= 1.0f && z >= 0.0f && z <= 1.0f) {
+		if (x >= -0.5f && x <= 0.5f && z >= -0.5f && z <= 0.5f) {
 			float distance = glm::distance(glm::vec3(x, y, z), localPos);
 			return check(button, callback, distance, block, glm::vec3(block.x, block.y + sign.y, block.z));
 		}
 	}
 	
 	if (vz) {
-		float x = (1.0f - lz) / vz * vx + lx;
-		float y = (1.0f - lz) / vz * vy + ly;
-		float z = 1.0f;
+		float x = (0.5f - lz) / vz * vx + lx;
+		float y = (0.5f - lz) / vz * vy + ly;
+		float z = 0.5f;
 
-		if (x >= 0.0f && x <= 1.0f && y >= 0.0f && y <= 1.0f) {
+		if (x >= -0.5f && x <= 0.5f && y >= -0.5f && y <= 0.5f) {
 			float distance = glm::distance(glm::vec3(x, y, z), localPos);
 			return check(button, callback, distance, block, glm::vec3(block.x, block.y, block.z + sign.z));
 		}
